7d56f5c05deedafee63e600bd7e203f4ead3ce45
suprad-parashar <suprad.s.parashar@gmail.com>
2020-03-29 15:30:15 +0530

    Main file created.

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import webbrowser as web
+
+get_developer_URLs = lambda index: "https://www.linkedin.com/in/supradparashar/" if index == 1 else "https://www.linkedin.com/in/sandeep-n-s-6b3888165/"
+
+def add_menu(window):
+    menu = Menu(window)
+    window.config(menu = menu)
+
+    files_menu = Menu(menu)
+    help_menu = Menu(menu)
+
+    files_menu.add_command(label = "Settings", command = print("#"))
+    files_menu.add_separator()
+    files_menu.add_command(label = "Exit", command = window.quit)
+
+    # TODO: Fix opening of the urls at the start of the program. It should open only if the button is clicked.
+    about_menu = Menu(help_menu)
+    about_menu.add_command(label = "Sandeep N S", command = web.open(get_developer_URLs(0)))
+    about_menu.add_command(label = "Suprad S Parashar", command = web.open(get_developer_URLs(1)))
+
+    help_menu.add_cascade(label = "About", menu = about_menu)
+
+    menu.add_cascade(label = "File", menu = files_menu)
+    menu.add_cascade(label = "Help", menu = help_menu)
+    web.
+
+window = Tk()
+add_menu(window)
+window.mainloop()
