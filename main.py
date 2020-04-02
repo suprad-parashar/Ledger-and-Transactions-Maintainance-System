740b0efe2cecea0b516c41915e5b1818aff5b356
sandy13521 <nandavarmukund1999gmail.com>
2020-04-03 01:00:45 +0530

    Add Transaction and Integration with Dashboard.

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from tkinter import *
+import tkinter.ttk as table
 import webbrowser as web
 import people
+import dashboard
 import transaction
 
-get_developer_URLs = lambda index: "https://www.linkedin.com/in/supradparashar/" if index == 1 else "https://www.linkedin.com/in/sandeep-n-s-6b3888165/"
 
+def change_frame(frame, name, prev = "None"):
+    global window, people_button, transactions_button
+    frame.destroy()
+    if name == "Dashboard":
+        if prev == "People":
+            people_button.config(text = "People", command = lambda: change_frame(frame, "People"))
+        if prev == "Transaction":
+            transactions_button.config(text = "Transaction", command = lambda: change_frame(frame,"Transaction"))
+
+    elif name == "People":
+        people_button.config(text = "Dashboard", command = lambda: change_frame(frame, "Dashboard", "People"))
+        frame = people.get_frame(window)
+        frame.pack(side = TOP)
+
+    elif name == "Transaction":
+        transactions_button.config(text="DashBoard",command = lambda :change_frame(frame,"Dashboard","Transaction"))
+        frame = transaction.get_frame(window)
+        frame.pack(side = TOP)
+
+
+# This method adds the menu to the program.
 def add_menu(window):
     menu = Menu(window)
     window.config(menu = menu)
@@ -16,19 +38,28 @@ def add_menu(window):
     files_menu.add_separator()
     files_menu.add_command(label = "Exit", command = window.quit)
 
-    # TODO: Fix opening of the urls at the start of the program. It should open only if the button is clicked.
     about_menu = Menu(help_menu)
-    about_menu.add_command(label = "Sandeep N S")#, command = web.open(get_developer_URLs(0)))
-    about_menu.add_command(label = "Suprad S Parashar")#, command = web.open(get_developer_URLs(1)))
+    about_menu.add_command(label = "Sandeep N S", command = lambda: web.open("https://www.linkedin.com/in/sandeep-n-s-6b3888165/"))
+    about_menu.add_command(label = "Suprad S Parashar", command = lambda: web.open("https://www.linkedin.com/in/supradparashar/"))
 
     help_menu.add_cascade(label = "About", menu = about_menu)
 
     menu.add_cascade(label = "File", menu = files_menu)
     menu.add_cascade(label = "Help", menu = help_menu)
 
+
+# Main Logic of the function
 window = Tk()
 window.title("Ledger and Transactions Maintainance System")
 add_menu(window)
-frame = transaction.get_frame(window)
-frame.pack()
+frame = dashboard.get_frame(window)
+frame.pack(side = TOP)
+navigation_frame = Frame(window)
+people_button = Button(navigation_frame, text = "People", command = lambda: change_frame(frame, "People"))
+transactions_button = Button(navigation_frame, text = "Transactions",command = lambda :change_frame(frame,"Transaction"))
+quit_button = Button(navigation_frame, text = "Quit", command = window.quit)
+people_button.pack(side = LEFT)
+transactions_button.pack(side = LEFT)
+quit_button.pack(side = LEFT)
+navigation_frame.pack(side = BOTTOM)
 window.mainloop()
--- transaction.py
+++ transaction.py
@@ -1,11 +1,4 @@
-"""
-Implemented By Sandeep N S (1JB17IS079)
-Transaction Module Functionalities are :
-    1. Add New Transactions.
-    2. Delete Old Transactions.
-    3. Displays Transaction that have previously happened.
-"""
-
+import os
 from tkinter import *
 import tkinter.ttk as table
 import hashlib as Hash
@@ -21,24 +14,96 @@ class Transaction:
         self.description = des
         self.amount = amount
         self.trans_date = trans_date
-        if trans_type==1:
+        if trans_type == 0:
             self.trans_type = "Debit"
         else:
             self.trans_type = "Credit"
 
+    def get_data(self):
+        return [
+            ("Sender Name", self.sender_name),
+            ("Amount", self.amount),
+            ("Description", self.description),
+            ("Transaction Type", "Debit" if self.trans_type == 0 else "Credit"),
+            ("Date of Transaction", self.trans_date)
+        ]
 
-def get_frame(window) :
+    def get_table_data(self):
+        return (self.sender_name, self.amount, self.trans_date, self.description, self.trans_type)
+
+    def get_data_frame(self, window):
+        frame = Frame(window, borderwidth=2, relief="raised")
+        details_table = table.Treeview(frame)
+        details_table["columns"] = ["parameter", "value"]
+        details_table["show"] = "headings"
+        index = 0
+        for data in self.get_data():
+            details_table.insert("", index, values=data)
+            index += 1
+        details_table.grid(row=0, column=0, columnspan=2)
+        return frame
+
+
+# Gets Data from People file.
+def get_people_list():
+    people_names = []
+    try:
+        with open("files/people.ltms", "rb") as f:
+
+            try:
+                while True:
+                    person = pickle.load(f)
+                    people_names.append(person)
+            except EOFError:
+                pass
+            people_names.sort(key=lambda person: person.name)
+    except FileNotFoundError:
+        with open("files/people.ltms", "wb") as _:
+            pass
+    return people_names
+
+
+# Reloads the Table Contents
+def refresh_table(trans_table):
+    for entry in trans_table.get_children():
+        trans_table.delete(entry)
+    with open("files/transaction.ltms", "rb") as file:
+        trans = []
+        try:
+            while True:
+                transaction = pickle.load(file)
+                trans.append(transaction)
+        except EOFError:
+            pass
+        trans.sort(key=lambda trans: trans.trans_date)
+        index = 0
+        for transaction in trans:
+            trans_table.insert("", index, values=transaction.get_table_data(), tags=transaction.trans_id)
+            index += 1
+
+
+def get_frame(window):
     frame = Frame(window)
+
+    # Create files if it does not exists.
+    try:
+        with open("files/transaction.ltms", "rb") as _, open("files/index_transaction.txt", "r") as _:
+            pass
+    except FileNotFoundError:
+        os.mkdir("files")
+        with open("files/transaction.ltms", "wb") as _, open("files/index_transaction.txt", "w") as _:
+            pass
 
     trans_table = table.Treeview(frame)
     trans_table.grid(row=0, column=0, columnspan=6)
-    trans_table["columns"] = ["name", "amount", "dot","des","type"]
+    trans_table["columns"] = ["name", "amount", "dot", "des", "type"]
     trans_table["show"] = "headings"
     trans_table.heading("name", text="Name")
     trans_table.heading("amount", text="Amount")
     trans_table.heading("dot", text="Date Of Transaction")
     trans_table.heading("des", text="Description")
-    trans_table.heading("type",text="Type")
+    trans_table.heading("type", text="Type")
+
     try:
         with open("files/transaction.ltms", "rb") as file:
             trans = []
@@ -52,19 +117,23 @@ def get_frame(window) :
             trans.sort(key=lambda person: a_trans.trans_date)
             index = 0
             for a_trans in trans:
-                trans_table.insert("", index, values=(a_trans.sender_name, a_trans.amount, a_trans.trans_date,a_trans.description,a_trans.trans_type))
+                trans_table.insert("", index, values=(
+                a_trans.sender_name, a_trans.amount, a_trans.trans_date, a_trans.description, a_trans.trans_type),
+                                   tags=a_trans.trans_id)
                 index += 1
     except FileNotFoundError:
-        with open("files/transaction.ltms", "wb") as file:
+        with open("files/transaction.ltms", "wb") as _:
             pass
 
-    add_button = Button(frame, text="Add Transaction", command=add_transaction)
-    delete_button = Button(frame, text="Delete Transaction")  # command = delete_transaction
-    view_button = Button(frame,text = "View Transaction",command = view_transaction)
+    add_button = Button(frame, text="Add Transaction", command=lambda: add_transaction(trans_table))
+    delete_button = Button(frame, text="Delete Transaction",
+                           command=lambda: delete_transaction(trans_table.item(trans_table.selection()[0]),
+                                                              trans_table))
+    view_button = Button(frame, text="View Transaction",
+                         command=lambda: view_transaction(trans_table.item(trans_table.selection()[0])))
     add_button.grid(row=1, column=0, columnspan=2)
     delete_button.grid(row=1, column=2, columnspan=2)
-    view_button.grid(row = 1, column=4, columnspan=2)
-
+    view_button.grid(row=1, column=4, columnspan=2)
     return frame
 
 
@@ -72,33 +141,91 @@ def writename(text):
     print(text)
 
 
-def add_transaction():
+def delete_transaction(item, trans_table):
+    print(item)
+    delete_id = item["tags"][0]
+    delete_index = 0
+    index = 0
+    with open("files/transaction.ltms", "rb") as trans_file, open("files/index_transaction.txt", "r") as index_file:
+        trans = []
+        indices = []
+        try:
+            while True:
+                transaction = pickle.load(trans_file)
+                entry = index_file.readline().split()
+                if transaction.trans_id == delete_id:
+                    delete_index = index
+                trans.append(transaction)
+                indices.append(entry)
+                index += 1
+        except EOFError:
+            pass
+    deleted_transaction = trans.pop(delete_index)
+    indices.pop(delete_index)
+    result = dialog.askquestion("Delete Transaction",
+                                "Do you want to delete the Transaction with {} from History?".format(
+                                    deleted_transaction.sender_name), icon='warning')
+    if result == 'yes':
+        with open("files/transaction.ltms", "wb") as trans_file, open("files/index_transaction.txt", "w") as index_file:
+            for i in range(len(trans)):
+                pickle.dump(trans[i], trans_file)
+                index_file.write(" ".join(indices[i]) + "\n")
+        dialog.showinfo("Deletion Successful",
+                        "The Transaction with person named {} on {} has been deleted from the record.".format(
+                            deleted_transaction.sender_name, deleted_transaction.trans_date))
+        refresh_table(trans_table)
 
-    #Saving Transactions into File
+
+def view_transaction(item):
+    person_id = item["tags"][0]
+    with open("files/transaction.ltms", "rb") as file:
+        while True:
+            person = pickle.load(file)
+            if person.trans_id == person_id:
+                break
+        trans_details_window = Tk()
+        transactions_frame = Frame(trans_details_window)
+        transactions_frame.grid(row=0, column=0, columnspan=2)
+        trans_frame = person.get_data_frame(trans_details_window)
+        trans_frame.grid(row=0, column=2)
+        close_button = Button(trans_frame, text="Close", command=trans_details_window.destroy)
+        close_button.grid(row=1, column=0,columnspan=2)
+        trans_details_window.mainloop()
+
+
+def add_transaction(trans_table):
+    # Saving Transactions into File
     def save_transaction():
-        sender_name = sname_var.get()
-        amount = amount_input.get()
-        des = des_input.get()
-        trans_type = typeOfTrans.get()
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        trans_date_and_time = dt_string
-        if amount == "0" or amount == "" :
-            dialog.showerror("Invalid Input", "Name cannot be empty.")
-        else:
+            sender_name = sname_var.get()
+            amount = amount_input.get()
+            des = des_input.get()
+            trans_type = typeOfTrans.get()
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            trans_date_and_time = dt_string
+
             trans_id = Hash.md5((sender_name + amount).encode()).hexdigest()
-            with open("files/transaction.ltms", "ab") as file:
-                with open("files/index_transaction.txt", "a") as index:
-                    index.write(trans_id + " " + str(file.tell()) + "\n")
-                trans = Transaction(trans_id, sender_name, des, amount, trans_date_and_time, trans_type)
-                pickle.dump(trans, file)
-            trans_sub_window.destroy()
+            trans = Transaction(trans_id, sender_name, des, amount, trans_date_and_time, trans_type)
 
+            #Verifying entered Amount
+            if amount == "0" or amount == "" or not amount.isdigit():
+                dialog.showerror("Invalid Input", "Enter Some Amount")
+            else:
+                #Asking for Confirmation
+                r = dialog.askquestion("Delete Transaction",
+                                       "Do you want to add this Transaction to record?", icon='warning')
+                if r == "yes":
+                    with open("files/transaction.ltms", "ab") as file:
+                        with open("files/index_transaction.txt", "a") as index:
+                            index.write(trans_id + " " + str(file.tell()) + "\n")
+                        pickle.dump(trans, file)
+                        refresh_table(trans_table)
+                        trans_sub_window.destroy()
 
-    #Variable for the Type Of Transaction(Debit Or Credit)
-    typeOfTrans = IntVar()
+                else:
+                    trans_sub_window.destroy()
 
-    #Transaction Window
+    # Transaction Window
     trans_sub_window = Tk()
     entry_var = StringVar()
     trans_sub_window.title("Add Transaction")
@@ -106,65 +233,47 @@ def add_transaction():
     bottom_frame = Frame(trans_sub_window)
     bottom_frame.pack(side=BOTTOM)
 
-    add_button = Button(bottom_frame, text="Add Transaction", command=save_transaction)
+    add_button = Button(bottom_frame, text="Add Transaction", command=lambda: save_transaction())
     cancel_button = Button(bottom_frame, text="Cancel", command=trans_sub_window.quit)
-    cancel_button.pack(side=TOP)
-    add_button.pack(side=TOP)
+    add_button.grid(row=6,column=0,columnspan=2)
+    cancel_button.grid(row=6,column=2,columnspan=2)
+
 
     top_frame = Frame(trans_sub_window)
     top_frame.pack(side=TOP)
 
-    #Adding The Labels And Input Fields (Entry)
+    # Adding The Labels And Input Fields (Entry)
     sname = Label(top_frame, text="Sender Name", textvariable=entry_var)
     sname.grid(row=0, column=0)
     sname_var = StringVar(top_frame)
-    # TODO: The Display the list of Names to choose from.
-    #Taking Input from File for List of People added by User.
-    try:
-        with open("files/people.ltms", "rb") as f:
-            people = []
-            try:
-                while True:
-                    person = pickle.load(f)
-                    people.append(person)
-            except EOFError:
-                pass
-            people.sort(key=lambda person: person.name)
-            print(people)
-    except FileNotFoundError :
-        with open("files/people.ltms", "wb") as file:
-            pass
+
+    options = get_people_list()
+    names = []
+    for i in options:
+        names.append(i.name)
+
+    sname_var.set(names[0])
+    sname_input = table.Combobox(top_frame, width=17, textvariable=sname_var, values=names)
+    sname_input.grid(row=0, column=1)
 
     amount = Label(top_frame, text="Amount (in Rs.)")
     amount.grid(row=1, column=0)
     amount_input = Entry(top_frame)
     amount_input.grid(row=1, column=1)
 
+    typeOfTrans = IntVar()
     trans_type = Label(top_frame, text="Transaction Type")
     trans_type.grid(row=4, column=0)
     trans_type_frame = Frame(top_frame)
-    debit_radio = Radiobutton(trans_type_frame, text="Debit", value = 1, variable=typeOfTrans, state=ACTIVE)
-    credit_radio = Radiobutton(trans_type_frame, text="Credit", value =2, variable=typeOfTrans, state=ACTIVE)
+    debit_radio = Radiobutton(trans_type_frame, text="Debit", value=0, variable=typeOfTrans, state=ACTIVE)
+    credit_radio = Radiobutton(trans_type_frame, text="Credit", value=1, variable=typeOfTrans, state=ACTIVE)
     debit_radio.pack(side=LEFT)
     credit_radio.pack(side=LEFT)
     trans_type_frame.grid(row=4, column=1)
 
-
     des = Label(top_frame, text="Description")
-    des.grid(row = 5, column = 0)
+    des.grid(row=5, column=0)
     des_input = Entry(top_frame)
     des_input.grid(row=5, column=1)
 
     trans_sub_window.mainloop()
-
-
-# Still in Progress.
-def view_transaction():
-    trans_history_sub_window = Tk()
-    trans_history_sub_window.title("View Transaction")
-
-    list = Listbox(trans_history_sub_window)
-    list.insert(1,"India")
-    list.pack()
-
-    trans_history_sub_window.mainloop()
